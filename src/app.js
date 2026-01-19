0cdd1e7f0542df15f8336cf5cdf2e864f4bd8816
jhonyes04 <jhonyes@hotmail.com>
2026-01-19 12:49:39 +0100

    Generador de dominios acabado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
-import "bootstrap";
-import "./style.css";
+import 'bootstrap';
+import './style.css';
 
+import './assets/img/rigo-baby.jpg';
+import './assets/img/4geeks.ico';
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const pronoun = ['the', 'our'];
+const adj = ['great', 'big'];
+const noun = ['jogger', 'racoon', 'pasiones', 'comsocom'];
+const extensions = ['es', 'com', 'org', 'net'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const domainHack = (palabra, extension) => {
+    return palabra.endsWith(extension)
+        ? `${palabra.slice(0, -extension.length)}.${extension}`
+        : `${palabra}.${extension}`;
+};
+
+window.onload = function () {
+    //write your code here
+    const listaDominios = document.getElementById('lista_dominios');
+
+    pronoun.map((p) => {
+        adj.map((a) => {
+            noun.map((n) => {
+                extensions.map((e) => {
+                    const option = document.createElement('option');
+                    let palabra = `${p}${a}${n}`;
+
+                    option.value = domainHack(palabra, e);
+                    option.textContent = option.value;
+                    listaDominios.appendChild(option);
+
+                    console.log(domainHack(palabra, e));
+                });
+            });
+        });
+    });
 };
